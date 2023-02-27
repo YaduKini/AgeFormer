fea8dd83bc925a7b84d4450f9a980f7e1795945a
Yadunandan Kini <yadunandan_kini@c1-mgmt.aim.in.tum.de>
2023-02-27 16:44:52 +0100

    datasets

--- datasets.py
+++ datasets.py
@@ -12,12 +12,15 @@ from torch.utils.data.sampler import SubsetRandomSampler
 
 class UKBBDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str='/home/guests/projects/ukbb/abdominal/nifti',
-                 target_dir: str='/home/guests/projects/ukbb/yadu/age_imaging_filtered_train_val.csv'):
+                 train_csv: str='/home/guests/projects/ukbb/yadu/tabular_data_train.csv',
+                 val_csv: str='/home/guests/projects/ukbb/yadu/tabular_data_val.csv'):
         super().__init__()
         self.data_dir = data_dir
 
         # length of train val csv is 35122, length of test csv is 8712
-        self.target_dir = target_dir
+        #self.target_dir = target_dir
+        self.train_csv = train_csv
+        self.val_csv = val_csv
         #self.batch_size = batch_size
         self.sets = parse_settings()
         #self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.0,), std=(1,))])
@@ -28,58 +31,53 @@ class UKBBDataModule(pl.LightningDataModule):
         if stage == "fit" or stage is None:
             if self.sets.phase == 'train':
                 # len of train val is 35122
-                ukbb_full = UKBB(self.data_dir, self.target_dir)
+                # len of train is 28125
+                # len of val is 6997
+
+                self.ukbb_train = UKBB(self.data_dir, self.train_csv)
+                self.ukbb_val = UKBB(self.data_dir, self.val_csv)
+                #ukbb_full = UKBB(self.data_dir, self.target_dir)
                 #print("type of ukbb full")
                 #print(type(ukbb_full))
-                ukbb_dataset_size = len(ukbb_full)
 
 
-                print("len ukbb full")
-                print(ukbb_dataset_size)
-                ################################## working code block ##########################
+                #ukbb_train_dataset_size = len(ukbb_train)
+                #ukbb_val_dataset_size = len(ukbb_val)
+                #print("len ukbb_train")
+                #print(ukbb_train_dataset_size)
+                #print("len ukbb_val")
+                #print(ukbb_val_dataset_size)
+
                 #train_val_size = int(35122)
                 #test_size = int(7000)
-                train_size = int(30000)
-                val_size = int(5122)
 
+                # change the value here to add more training data
+                #train_size = int(1536)
+                #remainder_train_size = ukbb_train_dataset_size - train_size
 
-                self.ukbb_train, self.ukbb_val = random_split(ukbb_full, [train_size, val_size])
+                # change the value here to add more val data
+                #val_size = int(384)
+                #remainder_val_size = ukbb_val_dataset_size - val_size
 
-                ######################## end of working code block##############################
+                ############## get training and val data set ################################################
 
-                ###################### begin code for training check ###########################
-                # code for training check
+                #self.ukbb_train, _ = random_split(ukbb_train, [train_size, remainder_train_size])
 
-                #check_dataset_size = int(5122)
-                #check_train_size = int(4000)
-                check_train_size = int(1122)
-                check_val_size = int(4000)
+                #self.ukbb_val, _ = random_split(ukbb_val, [val_size, remainder_val_size])
 
-                #self.check_train, self.check_val = random_split(self.ukbb_val, [check_train_size, check_val_size])
-                # do random split twice. in one get thr training set. in the other, get the validation set. 
-                
-
-                self.check_train, self.check_val = random_split(self.ukbb_val, [check_train_size, check_val_size])
-                
-
-                #self.check_train, self.check_val = random_split(check_train_val, [5000, 1000])
-                ##############################################################################################
-                #check_subset_size = int(1000)
-                #self.subset = self.check_test
-
-                self.subset_train, self.subset_val = random_split(self.check_val, [3200, 800])
+                #self.subset_train, self.subset_val = random_split(self.check_val, [160, 40])
                 #self.subset_train, self.subset_val = random_split(subset_train_val, [800, 100])
 
 
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
-        return DataLoader(self.subset_train, batch_size=self.sets.batch_size, shuffle=True, num_workers=4)
+        return DataLoader(self.ukbb_train, batch_size=self.sets.batch_size, shuffle=True, num_workers=4)
 
     def val_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.subset_val, batch_size=self.sets.batch_size, shuffle=False, num_workers=4)
+        return DataLoader(self.ukbb_val, batch_size=self.sets.batch_size, shuffle=False, num_workers=4)
 
-    def test_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.subset_test, batch_size=self.sets.batch_size, shuffle=False, num_workers=4)
+    # def test_dataloader(self) -> EVAL_DATALOADERS:
+    #     return DataLoader(self.subset_test, batch_size=self.sets.batch_size, shuffle=False, num_workers=4)
 
-    def predict_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.ukbb_full, batch_size=self.sets.batch_Size)
+    # def predict_dataloader(self) -> EVAL_DATALOADERS:
+    #     return DataLoader(self.ukbb_full, batch_size=self.sets.batch_Size)
