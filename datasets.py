6e0a71a9ec321d1ee681b2ff321024587c5f804e
YaduKini <yadu9199@gmail.com>
2023-09-21 02:08:32 +0000

    dataset loader commit cleaned

--- datasets.py
+++ datasets.py
@@ -11,73 +11,57 @@ import math
 from torch.utils.data.sampler import SubsetRandomSampler
 
 class UKBBDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str='/home/guests/projects/ukbb/abdominal/nifti',
-                 train_csv: str='/home/guests/projects/ukbb/yadu/tabular_data_train.csv',
-                 val_csv: str='/home/guests/projects/ukbb/yadu/tabular_data_val.csv'):
+    def __init__(self, data_dir: str='/vol/aimspace/projects/ukbb/abdominal/nifti',
+                 train_csv: str='/vol/aimspace/users/kini/yadu/sex_prediction/tabular_data_train_aimspace.csv',
+                 val_csv: str='/vol/aimspace/users/kini/yadu/sex_prediction/tabular_data_val_aimspace.csv',
+                 test_csv: str='/vol/aimspace/users/kini/yadu/sex_prediction/tabular_data_test_aimspace.csv'):
         super().__init__()
         self.data_dir = data_dir
 
-        # length of train val csv is 35122, length of test csv is 8712
-        #self.target_dir = target_dir
         self.train_csv = train_csv
         self.val_csv = val_csv
-        #self.batch_size = batch_size
+        self.test_csv = test_csv
         self.sets = parse_settings()
-        #self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.0,), std=(1,))])
     
 
     def setup(self, stage: str = None) -> None:
         # assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
             if self.sets.phase == 'train':
-                # len of train val is 35122
-                # len of train is 28125
-                # len of val is 6997
 
                 self.ukbb_train = UKBB(self.data_dir, self.train_csv)
                 self.ukbb_val = UKBB(self.data_dir, self.val_csv)
-                #ukbb_full = UKBB(self.data_dir, self.target_dir)
-                #print("type of ukbb full")
-                #print(type(ukbb_full))
 
+                # code below is for sanity check using one data sample
+
+                #train_size = int(2)
 
                 #ukbb_train_dataset_size = len(ukbb_train)
+                #print("length of ukbb train dataset size: ", ukbb_train_dataset_size)
+
                 #ukbb_val_dataset_size = len(ukbb_val)
-                #print("len ukbb_train")
-                #print(ukbb_train_dataset_size)
-                #print("len ukbb_val")
-                #print(ukbb_val_dataset_size)
+                #print("length of ukbb val dataset size: ", ukbb_val_dataset_size)
 
-                #train_val_size = int(35122)
-                #test_size = int(7000)
-
-                # change the value here to add more training data
-                #train_size = int(1536)
                 #remainder_train_size = ukbb_train_dataset_size - train_size
 
-                # change the value here to add more val data
-                #val_size = int(384)
+                #val_size = int(1)
                 #remainder_val_size = ukbb_val_dataset_size - val_size
 
-                ############## get training and val data set ################################################
-
                 #self.ukbb_train, _ = random_split(ukbb_train, [train_size, remainder_train_size])
-
                 #self.ukbb_val, _ = random_split(ukbb_val, [val_size, remainder_val_size])
 
-                #self.subset_train, self.subset_val = random_split(self.check_val, [160, 40])
-                #self.subset_train, self.subset_val = random_split(subset_train_val, [800, 100])
+
+        if stage == "inference":
+            if self.sets.phase == "test":
+                self.ukbb_test = UKBB(self.data_dir, self.test_csv)
 
 
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
-        return DataLoader(self.ukbb_train, batch_size=self.sets.batch_size, shuffle=True, num_workers=4)
+        return DataLoader(self.ukbb_train, batch_size=self.sets.batch_size, shuffle=True, num_workers=8)
 
     def val_dataloader(self) -> EVAL_DATALOADERS:
-        return DataLoader(self.ukbb_val, batch_size=self.sets.batch_size, shuffle=False, num_workers=4)
+        return DataLoader(self.ukbb_val, batch_size=self.sets.batch_size, shuffle=False, num_workers=8)
 
-    # def test_dataloader(self) -> EVAL_DATALOADERS:
-    #     return DataLoader(self.subset_test, batch_size=self.sets.batch_size, shuffle=False, num_workers=4)
-
-    # def predict_dataloader(self) -> EVAL_DATALOADERS:
-    #     return DataLoader(self.ukbb_full, batch_size=self.sets.batch_Size)
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.ukbb_test, batch_size=self.sets.batch_size, shuffle=True, num_workers=8)
